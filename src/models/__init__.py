e8dc3ab4465565be9d2b4aa1c293f06a19770746
Dmitrii Isaev <dimais@mail.ru>
2026-07-08 17:49:25 +0700

    feat: add ORM models

---
+++ src/models/__init__.py
@@ -0,0 +1,7 @@
+__all__ = ["Base", "Check", "Document", "Issue"]
+
+
+from models.base import Base
+from models.check import Check
+from models.document import Document
+from models.issue import Issue
